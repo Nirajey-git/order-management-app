9db53a8e3908fc83819bfa3906e53649c86c39d0
Niraj Gajurel <nirazgajurel21@gmail.com>
2025-10-04 20:40:43 +1000

    prisma update

--- backend/src/prisma.js
+++ backend/src/prisma.js
@@ -1,5 +1,9 @@
 import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve("../prisma/.env") });
 
 const prisma = new PrismaClient();
 export default prisma;
